ee85f82d3d76248a6cc644e4211ec0162f7f841e
Bernardeschi <kkostik100@yandex.ru>
2023-11-17 12:27:57 +0500

    add GiftSender and bagfix

--- Acc.php
+++ Acc.php
@@ -1,7 +1,13 @@
 <?php
 
 class Account {
-    public function __construct($login, $password) {
+    private $login;
+    private $password;
+    private $token;
+    public $user_id;
+    public $userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0' . "\r\n";
+
+    public function __construct(string $login, string $password) {
         $this->login = $login;
         $this->password = $password;
 
@@ -16,11 +22,20 @@ class Account {
             '2fa_supported'=>'1'
         );
 
-        $content = file_get_contents('http://oauth.vk.com/token?' . http_build_query($params));
-        $response = json_decode($content);
+        $options = array(
+            'http'=>array(
+                'method' => 'GET',
+                'user_agent' => $this->userAgent
+            )
+        );
 
-        $this->token = $response->access_token;
-        $this->user_id = $response->user_id;
+        $context = stream_context_create($options);
+
+        $content = file_get_contents('http://oauth.vk.com/token?' . http_build_query($params), false, $context);
+        $response = json_decode($content, true);
+
+        $this->token = isset($response['access_token']) ? $response['access_token'] : '';
+        $this->user_id = isset($response['user_id']) ? $response['user_id'] : '';
     }
 
     function getToken() {
@@ -29,5 +44,20 @@ class Account {
 
     function getUserId() {
         return $this->user_id;
+    }
+
+    function giftSender(string $user_id, string $gift_id, string $text) : string
+    {
+        $params = array(
+            'user_ids' => $user_id,
+            'gift_id' => $gift_id,
+            'message' => $text,
+            'guid' => rand(1000, 2000),
+            'access_token' => $this->token,
+            'v' => '5.81'
+        );
+        
+        $request = file_get_contents('https://api.vk.com/method/gifts.send?' . http_build_query($params));
+        return $request;
     }
 }
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+ini_set('max_execution_time', 60000);
 $_SESSION['account'] = (isset($_SESSION['account'])) ? $_SESSION['account'] : '';
 $_SESSION['accounts'] = (isset($_SESSION['accounts'])) ? $_SESSION['accounts'] : '';
 
@@ -10,6 +11,10 @@ $loader = new Twig\Loader\FilesystemLoader('templates');
 $twig = new Twig\Environment($loader);
 
 $result = array();
+$newLoginPass = '';
+$ids_for_gifts = file_get_contents('ids.txt');
+$_SESSION['ids'] = (strlen($ids_for_gifts) > 1) ? $ids_for_gifts : '';
+
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
     if(isset($_POST['joinGroups'])) {
@@ -32,18 +37,21 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
             'https://vk.com/club',
             'vk.com/club'
         );
+
+        $options  = array('http' => array('method' => 'GET', 'user_agent' => $acc->userAgent));
+        $context  = stream_context_create($options);
     
         foreach(explode("\n", $groups) as $group) {
             $group = str_replace($exeption , '', $group);
             $params['group_id'] = $group;
-            $request = file_get_contents('http://api.vk.com/method/groups.join?' . http_build_query($params));
+            $request = file_get_contents('http://api.vk.com/method/groups.join?' . http_build_query($params), false, $context);
             $response = json_decode($request, true);
             if(isset($response['error'])) {
                 $result['club' . $group] = $response['error']['error_msg'];
             } else {
                 $result['club' . $group] = 'Успешно!';
             }
-            sleep(2);
+            sleep(3);
         };
     };
 
@@ -146,11 +154,94 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         };
         
     };
-          
+
+    // if(isset($_POST['passChange']))
+    // проверяем нажатие кнопки смены пароля
+
+    if(isset($_POST['passChange'])) {
+        $account = $_POST['account'];
+        $newPass = $_POST['newPass'];
+
+        $arrayLoginPass = explode(':', $account);
+        $acc = new Account($arrayLoginPass[0], $arrayLoginPass[1]);
+
+        $userId = $acc->getUserId();
+
+        $params = array(
+            'access_token'=>$acc->getToken(),
+            'v'=>'5.81',
+            'old_password'=>$arrayLoginPass[1],
+            'new_password'=>$newPass
+        );
+
+        $url = 'https://api.vk.com/method/account.changePassword?' . http_build_query($params);
+        $request = file_get_contents($url);
+        $response = json_decode($request, true);
+
+        $newToken = $response['response']['token'];
+        $newLoginPass = $arrayLoginPass[0] . ':' . $newPass;
+
+        $log = date('Y-m-d H:i:s') . ' смена пароля ' . 'id' . $userId . ': ' . $newLoginPass . PHP_EOL . $newToken . ';' . $userId . PHP_EOL . '==========';
+        file_put_contents('log.txt', $log . PHP_EOL, FILE_APPEND);
+
+    };
+
+    // Отправка подарков
+    // Проверяем нажатие кнопки gifts
+
+    if (isset($_POST['gifts'])) {
+        $accounts = explode("\r\n", $_POST['accounts']);
+        $gift_id = $_POST['gift_id'];
+        $text = $_POST['text'];
+
+        // CREATE ACCOUNT OBJECT
+        $acc = array_pop($accounts);
+        [$login, $password] = explode(':', $acc);
+        $accObj = new Account($login, $password);
+
+        $search = array(
+            'https://vk.com/id',
+            'https://vk.com/'
+        );
+        
+        foreach(explode("\r\n", $_POST['users']) as $user_id) {
+            $user_id = str_replace($search, '', $user_id);
+            while(count($accounts) >= 0) {
+                $response = json_decode($accObj->giftSender($user_id, $gift_id, $text), true);
+                if(isset($response['error'])) {
+                    $result['id' . $accObj->user_id] = $response['error']['error_msg'];
+                    if(count($accounts) == 0) {
+                        break 2;
+                    } else {
+                        $acc = array_pop($accounts);
+                        [$login, $password] = explode(':', $acc);
+                        $accObj = new Account($login, $password);
+                    }    
+                } else {
+                    $result['id' . $user_id] = 'Успешно!';
+                    break;
+                }
+            }
+        }
+    }
 }
+
+function passGen() {
+    $chars = 'qazxswedcvfrtgbnhyujmkiolp1234567890QAZXSWEDCVFRTGBNHYUJMKIOLP';
+    $password = '';
+    $charsLen = strlen($chars)-1;
+    for($i = 0; $i < 8; $i++) {
+        $password .= $chars[rand(0, $charsLen)];
+    }
+
+    return $password;
+};
 
 echo $twig->render('index.html', array(
     'accounts'=>$_SESSION['accounts'],
     'account'=>$_SESSION['account'],
-    'result'=>$result
+    'ids'=>$_SESSION['ids'],
+    'result'=>$result,
+    'newLoginPass'=>$newLoginPass,
+    'passGen'=>passGen()
 ) );
